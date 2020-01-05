d64e0bfcc5fdfb147c8910116278fa00e381cf9d
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-05 18:53:11 +0100

    backend: added IAsyncRepository & ISpecification interfaces to core

---
+++ src/backend/Hotel5000_Api/Core/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(ISpecification<T> specification);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
