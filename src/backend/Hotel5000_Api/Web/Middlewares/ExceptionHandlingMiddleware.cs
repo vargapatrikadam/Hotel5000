e04aa9fdd794fa91433354e7d358f7180a7dd646
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-05 19:54:09 +0100

    backend: implemented central exception handler in web with logging

---
+++ src/backend/Hotel5000_Api/Web/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Core.Enums.Logging;
+using Core.Interfaces.Logging;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Web.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILoggingService loggingService;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILoggingService LoggingService)
+        {
+            this.next = next;
+            this.loggingService = LoggingService;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        private async Task HandleException(HttpContext context, Exception ex)
+        {
+            HttpStatusCode code = HttpStatusCode.InternalServerError; // 500 if unexpected
+
+            //Get innermost exception
+            while (ex.InnerException != null) ex = ex.InnerException;
+
+            // Specify different custom exceptions here
+            if (ex is ArgumentException) code = HttpStatusCode.BadRequest;
+
+            string result = JsonConvert.SerializeObject(new { error = ex.Message });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            LogLevel level = LogLevel.Warning;
+            if ((int)code == 500) level = LogLevel.Critical;
+            await loggingService.Log(ex.Message, level);
+
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
+}
--- src/backend/Hotel5000_Api/Web/Startup.cs
+++ src/backend/Hotel5000_Api/Web/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Web.Middlewares;
 
 namespace Web
 {
@@ -54,6 +55,8 @@ namespace Web
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseMiddleware(typeof(ExceptionHandlingMiddleware));
 
             app.UseEndpoints(endpoints =>
             {
