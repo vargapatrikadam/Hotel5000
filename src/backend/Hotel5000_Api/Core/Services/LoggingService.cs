35689de257de63b9a66e9f1531110403a2c9365a
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-05 18:57:39 +0100

    backend: implemented ILoggingService

---
+++ src/backend/Hotel5000_Api/Core/Services/LoggingService.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Logging;
+using Core.Enums.Logging;
+using Core.Interfaces;
+using Core.Interfaces.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    public class LoggingService : ILoggingService
+    {
+        private readonly IAsyncRepository<LogEntity> logRepository;
+        public LoggingService(IAsyncRepository<LogEntity> LogRepository)
+        {
+            logRepository = LogRepository;
+        }
+        public async void Log(LogEntity log)
+        {
+            await logRepository.AddAsync(log);
+        }
+
+        public async void Log(string message, LogLevel type)
+        {
+            await logRepository.AddAsync(new LogEntity() { Message = message, Timestamp = DateTime.Now, Type = type });
+        }
+    }
+}
