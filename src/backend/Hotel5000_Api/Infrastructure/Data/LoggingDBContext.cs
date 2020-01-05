eb2cd8bbe7bb256bc4ea4417188944dc0ede84b2
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-05 19:06:07 +0100

    backend: added LoggingDBContext to infrastructure

--- src/backend/Hotel5000_Api/Infrastructure/Data/LoggingDBContext.cs
+++ src/backend/Hotel5000_Api/Infrastructure/Data/LoggingDBContext.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Core.Entities.Logging;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace Infrastructure.Data
 {
-    public class LoggingDBContext
+    public class LoggingDBContext : DbContext
     {
+        public LoggingDBContext(DbContextOptions<LoggingDBContext> options) : base(options) { }
+        public DbSet<LogEntity> Logs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
