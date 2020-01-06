6a8382634869925fe67ac0932f5276c011ddcd2a
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-06 11:45:42 +0100

    backend: made abstract repository class

---
+++ src/backend/Hotel5000_Api/Infrastructure/Data/Repositories/ARepository.cs
@@ -0,0 +1,55 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public abstract class ARepository<TEntity, TContext> : IAsyncRepository<TEntity> where TEntity : BaseEntity
+                                                                                     where TContext : DbContext
+    {
+        protected readonly TContext context;
+        protected ARepository(TContext Context)
+        {
+            context = Context;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            var newEntity = await context.Set<TEntity>().AddAsync(entity);
+            await context.SaveChangesAsync();
+            return newEntity.Entity;
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<TEntity>> GetAllAsync()
+        {
+            return await context.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<TEntity>> GetAsync(ISpecification<TEntity> specification)
+        {
+            return await ApplySpecification(specification).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            var data = context.Set<TEntity>().Update(entity).Entity;
+            context.Entry(data).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return data;
+        }
+        private IQueryable<TEntity> ApplySpecification(ISpecification<TEntity> specification)
+        {
+            return SpecificationEvaluator<TEntity>.GetQuery(context.Set<TEntity>().AsQueryable(), specification);
+        }
+    }
+}
--- src/backend/Hotel5000_Api/Infrastructure/Data/Repositories/LoggingDBRepository.cs
+++ src/backend/Hotel5000_Api/Infrastructure/Data/Repositories/LoggingDBRepository.cs
@@ -10,46 +10,12 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Data.Repositories
 {
-    public class LoggingDBRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    public class LoggingDBRepository<TEntity> : ARepository<TEntity, LoggingDBContext> 
+        where TEntity : BaseEntity
     {
-        protected readonly LoggingDBContext context;
-        public LoggingDBRepository(LoggingDBContext Context)
+        public LoggingDBRepository(LoggingDBContext context) : base(context)
         {
-            context = Context;
-        }
-        public async Task<T> AddAsync(T entity)
-        {
-            var newEntity = await context.Set<T>().AddAsync(entity);
-            await context.SaveChangesAsync();
-            return newEntity.Entity;
-        }
 
-        public async Task DeleteAsync(T entity)
-        {
-            context.Set<T>().Remove(entity);
-            await context.SaveChangesAsync();
-        }
-
-        public async Task<IReadOnlyList<T>> GetAllAsync()
-        {
-            return await context.Set<T>().AsNoTracking().ToListAsync();
-        }
-
-        public async Task<IReadOnlyList<T>> GetAsync(ISpecification<T> specification)
-        {
-            return await ApplySpecification(specification).AsNoTracking().ToListAsync();
-        }
-
-        public async Task<T> UpdateAsync(T entity)
-        {
-            var data = context.Set<T>().Update(entity).Entity;
-            context.Entry(data).State = EntityState.Modified;
-            await context.SaveChangesAsync();
-            return data;
-        }
-        private IQueryable<T> ApplySpecification(ISpecification<T> specification)
-        {
-            return SpecificationEvaluator<T>.GetQuery(context.Set<T>().AsQueryable(), specification);
         }
     }
 }
