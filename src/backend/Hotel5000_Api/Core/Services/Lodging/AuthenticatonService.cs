588874577e4f57c4024f568cfae10f1041b779b6
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-26 16:20:00 +0100

    backend: added password hash checker & password hasher to authenticate

--- src/backend/Hotel5000_Api/Core/Services/Lodging/AuthenticatonService.cs
+++ src/backend/Hotel5000_Api/Core/Services/Lodging/AuthenticatonService.cs
@@ -2,6 +2,7 @@
 using Core.Helpers;
 using Core.Interfaces;
 using Core.Interfaces.Lodging;
+using Core.Interfaces.PasswordHasher;
 using Core.Specifications;
 using System;
 using System.Collections.Generic;
@@ -14,15 +15,17 @@ namespace Core.Services.Lodging
     public class AuthenticatonService : IAuthenticaton
     {
         private readonly IAsyncRepository<User> UserRepository;
-        public AuthenticatonService(IAsyncRepository<User> userRepository)
+        private readonly IPasswordHasher PasswordHasher;
+        public AuthenticatonService(IAsyncRepository<User> userRepository, IPasswordHasher passwordHasher)
         {
             UserRepository = userRepository;
+            PasswordHasher = passwordHasher;
         }
         public async Task<User> AuthenticateAsync(string username, string password, string email)
         {
             User user = (await UserRepository.GetAsync(
                 new Specification<User>()
-                .ApplyFilter(p => (p.Email == email || p.Username == username) && p.Password == password)))
+                .ApplyFilter(p => (p.Email == email || p.Username == username) && PasswordHasher.Check(p.Password, password))))
                 .FirstOrDefault();
 
             //This way we don't need to implement a replica of ThenInclude from EF Core, because we cause eager loading on entities from the context.
@@ -40,6 +43,8 @@ namespace Core.Services.Lodging
             user.Password.ValidatePassword(out errorMessage);
             if (errorMessage != null)
                 throw new ArgumentException(errorMessage);
+
+            user.Password = PasswordHasher.Hash(user.Password);
 
             await UserRepository.AddAsync(user);
         }
