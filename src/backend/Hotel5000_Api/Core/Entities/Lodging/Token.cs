245ee3003cbceecac6ccc4d31a7d3deebe031083
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-26 16:35:49 +0100

    backend: added token to entities

---
+++ src/backend/Hotel5000_Api/Core/Entities/Lodging/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Lodging
+{
+    public class Token : BaseEntity
+    {
+        public string RefreshToken { get; set; }
+        
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
--- src/backend/Hotel5000_Api/Core/Entities/Lodging/User.cs
+++ src/backend/Hotel5000_Api/Core/Entities/Lodging/User.cs
@@ -19,5 +19,6 @@ namespace Core.Entities.Lodging
         public virtual ApprovingData ApprovingData { get; set; }
         public virtual ICollection<Contact> Contacts { get; set; }
         public virtual ICollection<Lodging> Lodgings { get; set; }
+        public virtual ICollection<Token> Tokens { get; set; }
     }
 }
