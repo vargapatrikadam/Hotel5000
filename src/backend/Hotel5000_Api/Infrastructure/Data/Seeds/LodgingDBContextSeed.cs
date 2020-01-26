3297205de7d70e057f4ab369c4c1cba576153afd
vargapatrikadam <patrikadamvarga@gmail.com>
2020-01-26 22:05:55 +0100

    backend: implemented domain database seeding

---
+++ src/backend/Hotel5000_Api/Infrastructure/Data/Seeds/LodgingDBContextSeed.cs
@@ -0,0 +1,56 @@
+﻿using Core.Entities.LodgingEntities;
+using Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Seeds
+{
+    public static class LodgingDBContextSeed
+    {
+        public static async Task SeedAsync(LodgingDBContext Context, bool isProduction)
+        {
+            //Only run this if using a real database
+            if(isProduction)
+                Context.Database.Migrate();
+
+            if (!Context.Roles.Any())
+            {
+                Context.Roles.AddRange(
+                    GetPreconfiguredRoles());
+
+                await Context.SaveChangesAsync();
+            }
+
+            if (!Context.PaymentTypes.Any())
+            {
+                Context.PaymentTypes.AddRange(
+                    GetPreconfiguredPaymentTypes());
+
+                await Context.SaveChangesAsync();
+            }
+        }
+        private static IEnumerable<Role> GetPreconfiguredRoles()
+        {
+            return new List<Role>()
+            {
+                new Role() { Name = "APPROVED_USER"},
+                new Role() { Name = "COMPANY"},
+                new Role() { Name = "ADMIN"}
+            };
+        }
+        private static IEnumerable<PaymentType> GetPreconfiguredPaymentTypes()
+        {
+            return new List<PaymentType>()
+            {
+                new PaymentType() { Name = "CASH"},
+                new PaymentType() { Name = "BANKCARD"},
+                new PaymentType() { Name = "CASH_PART"},
+                new PaymentType() { Name = "BANKCARD_PART"}
+            };
+        }
+    }
+}
--- src/backend/Hotel5000_Api/Web/Program.cs
+++ src/backend/Hotel5000_Api/Web/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data.Contexts;
+using Infrastructure.Data.Seeds;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +14,25 @@ namespace Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args).Build().Run();
+        //}
+
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var enviroment = services.GetRequiredService<Microsoft.AspNetCore.Hosting.IHostingEnvironment>();
+                var context = services.GetRequiredService<LodgingDBContext>();
+
+                await LodgingDBContextSeed.SeedAsync(context, enviroment.IsProduction());
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
