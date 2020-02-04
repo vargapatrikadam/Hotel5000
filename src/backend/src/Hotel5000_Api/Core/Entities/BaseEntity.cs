30e4f23548198cce6b761be067908394ccb09175
vargapatrikadam <patrikadamvarga@gmail.com>
2020-02-04 12:28:04 +0100

    backend: removed added at column

--- src/backend/src/Hotel5000_Api/Core/Entities/BaseEntity.cs
+++ src/backend/src/Hotel5000_Api/Core/Entities/BaseEntity.cs
@@ -7,7 +7,7 @@ namespace Core.Entities
     public abstract class BaseEntity
     {
         public int Id { get; set; }
-        public DateTime AddedAt { get; set; }
+        //public DateTime? AddedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
     }
 }
