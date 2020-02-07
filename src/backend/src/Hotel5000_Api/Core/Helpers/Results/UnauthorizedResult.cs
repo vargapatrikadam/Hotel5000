4fcb891e70c7fc23112aac7f3c88666c5ddb266f
vargapatrikadam <patrikadamvarga@gmail.com>
2020-02-07 22:07:14 +0100

    backend: implemented result pattern to core

---
+++ src/backend/src/Hotel5000_Api/Core/Helpers/Results/UnauthorizedResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Helpers.Results
+{
+    public class UnauthorizedResult<T> : AResult<T>
+    {
+        private readonly string[] _messages;
+        public UnauthorizedResult(params string[] messages)
+        {
+            _messages = messages;
+        }
+        public override ResultType ResultType => ResultType.Unauthorized;
+
+        public override List<string> Errors => _messages.ToList() ?? new List<string> { "Unauthorized" };
+
+        public override T Data => default;
+    }
+}
---
+++ src/backend/src/Hotel5000_Api/Core/Helpers/Results/UnexpectedResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Helpers.Results
+{
+    public class UnexpectedResult<T> : AResult<T>
+    {
+        private readonly string[] _messages;
+        public UnexpectedResult(params string[] messages)
+        {
+            _messages = messages;
+        }
+        public override ResultType ResultType => ResultType.Unexpected;
+        public override List<string> Errors => _messages.ToList() ?? new List<string> { "Unexpected error" };
+        public override T Data => default;
+    }
+}
