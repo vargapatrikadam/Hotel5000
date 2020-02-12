f85196eba8f22bc83b2bf1869f34eb38569e0cab
vargapatrikadam <patrikadamvarga@gmail.com>
2020-02-12 20:21:13 +0100

    backend: added logging to exception handler

--- src/backend/src/Hotel5000_Api/Web/Controllers/ErrorController.cs
+++ src/backend/src/Hotel5000_Api/Web/Controllers/ErrorController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Enums.Logging;
+using Core.Interfaces.Logging;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,25 +14,38 @@ namespace Web.Controllers
     [ApiController]
     public class ErrorController : ControllerBase
     {
+        private readonly ILoggingService _logger;
+        public ErrorController(ILoggingService logger)
+        {
+            _logger = logger;
+        }
         [ApiExplorerSettings(IgnoreApi = true)]
         [Route("/api/error")]
-        public IActionResult Error()
+        public async Task<IActionResult> Error()
         {
             var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
             Exception ex = context.Error;
+
             //handle internal server errors
+
+            await _logger.Log(context.Error.Message, LogLevel.Critical);
+
             return Problem();
         }
         [ApiExplorerSettings(IgnoreApi = true)]
         [Route("/api/error-local-development")]
-        public IActionResult ErrorLocalDevelopment([FromServices] IWebHostEnvironment webHostEnvironment)
+        public async Task<IActionResult> ErrorLocalDevelopment([FromServices] IWebHostEnvironment webHostEnvironment)
         {
             if (webHostEnvironment.EnvironmentName != "Development")
             {
-                throw new InvalidOperationException("This shouldn't be invoked in non-development environments.");
+                string msg = "This shouldn't be invoked in non-development environments.";
+                await _logger.Log(msg, LogLevel.Critical);
+                throw new InvalidOperationException(msg);
             }
 
             var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            await _logger.Log(context.Error.Message, LogLevel.Critical);
 
             return Problem(
                 detail: context.Error.StackTrace,
