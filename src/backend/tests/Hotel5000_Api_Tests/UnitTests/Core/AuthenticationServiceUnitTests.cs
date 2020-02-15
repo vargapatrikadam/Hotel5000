1855c1f29be679255101dbbace27afd6bad49beb
vargapatrikadam <patrikadamvarga@gmail.com>
2020-02-15 22:44:14 +0100

    backend: added authenticate unit tests

---
+++ src/backend/tests/Hotel5000_Api_Tests/UnitTests/Core/AuthenticationServiceUnitTests.cs
@@ -0,0 +1,99 @@
+﻿using Core.Entities.LodgingEntities;
+using Core.Helpers.Results;
+using Core.Interfaces;
+using Core.Interfaces.Lodging;
+using Core.Interfaces.PasswordHasher;
+using Core.Services.Lodging;
+using Core.Specifications;
+using Hotel5000_Api_Tests.UnitTests.Core.Data;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hotel5000_Api_Tests.UnitTests.Core
+{
+    public class AuthenticationServiceUnitTests
+    {
+        [Fact]
+        public async Task Authenticate_InvalidUser()
+        {
+            var mockUserRepo = new Mock<IAsyncRepository<User>>();
+            mockUserRepo.Setup(s => s.GetAsync(It.IsAny<ISpecification<User>>()))
+                .ReturnsAsync(new List<User>());
+            var mockTokenRepo = new Mock<IAsyncRepository<Token>>();
+            var mockRoleRepo = new Mock<IAsyncRepository<Role>>();
+            var mockPasswordHasher = new Mock<IPasswordHasher>();
+            var mockAuthenticationOptions = new Mock<ISetting<AuthenticationOptions>>();
+            mockAuthenticationOptions.Setup(s => s.Option)
+                .Returns(new AuthenticationOptions()
+                {
+                    AccessTokenDuration = 60,
+                    RefreshTokenDuration = 60,
+                    Secret = "Unit Test Secret"
+                });
+            var service = new AuthenticationService(mockUserRepo.Object, mockTokenRepo.Object, mockRoleRepo.Object, mockPasswordHasher.Object, mockAuthenticationOptions.Object);
+
+            var result = await service.AuthenticateAsync("asd", "asd", "asd");
+
+            Assert.IsType<UnauthorizedResult<User>>(result);
+        }
+
+        [Fact]
+        public async Task Authenticate_BadPassword()
+        {
+            var mockUserRepo = new Mock<IAsyncRepository<User>>();
+            mockUserRepo.Setup(s => s.GetAsync(It.IsAny<ISpecification<User>>()))
+                .ReturnsAsync(new List<User>() { AuthenticationEntities.GetTestUser() });
+            var mockTokenRepo = new Mock<IAsyncRepository<Token>>();
+            var mockRoleRepo = new Mock<IAsyncRepository<Role>>();
+            var mockPasswordHasher = new Mock<IPasswordHasher>();
+            mockPasswordHasher.Setup(s => s.Check(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(false);
+            var mockAuthenticationOptions = new Mock<ISetting<AuthenticationOptions>>();
+            mockAuthenticationOptions.Setup(s => s.Option)
+                .Returns(new AuthenticationOptions()
+                {
+                    AccessTokenDuration = 60,
+                    RefreshTokenDuration = 60,
+                    Secret = "Unit Test Secret"
+                });
+            var service = new AuthenticationService(mockUserRepo.Object, mockTokenRepo.Object, mockRoleRepo.Object, mockPasswordHasher.Object, mockAuthenticationOptions.Object);
+
+            var result = await service.AuthenticateAsync("testusername", "badpassword", "testemail");
+
+            Assert.IsType<UnauthorizedResult<User>>(result);
+        }
+        [Fact]
+        public async Task Authenticate_Successful()
+        {
+            var mockUserRepo = new Mock<IAsyncRepository<User>>();
+            mockUserRepo.Setup(s => s.GetAsync(It.IsAny<ISpecification<User>>()))
+                .ReturnsAsync(new List<User>() { AuthenticationEntities.GetTestUser() });
+            var mockTokenRepo = new Mock<IAsyncRepository<Token>>();
+            Token insertedToken = null;
+            mockTokenRepo.Setup(s => s.AddAsync(It.IsAny<Token>()))
+                .Callback((Token t) => insertedToken = t);
+            var mockRoleRepo = new Mock<IAsyncRepository<Role>>();
+            var mockPasswordHasher = new Mock<IPasswordHasher>();
+            mockPasswordHasher.Setup(s => s.Check(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+            var mockAuthenticationOptions = new Mock<ISetting<AuthenticationOptions>>();
+            mockAuthenticationOptions.Setup(s => s.Option)
+                .Returns(new AuthenticationOptions()
+                {
+                    AccessTokenDuration = 60,
+                    RefreshTokenDuration = 60,
+                    Secret = "Unit Test Secret"
+                });
+            var service = new AuthenticationService(mockUserRepo.Object, mockTokenRepo.Object, mockRoleRepo.Object, mockPasswordHasher.Object, mockAuthenticationOptions.Object);
+
+            var result = await service.AuthenticateAsync("testusername", "testpassword", "testemail");
+
+            Assert.IsType<SuccessfulResult<User>>(result);
+            Assert.True(insertedToken.UserId == 1);
+        }
+    }
+}
---
+++ src/backend/tests/Hotel5000_Api_Tests/UnitTests/Core/Data/AuthenticationEntities.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities.LodgingEntities;
+using Core.Enums.Lodging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel5000_Api_Tests.UnitTests.Core.Data
+{
+    public static class AuthenticationEntities
+    {
+        public static User GetTestUser()
+        {
+            return new User()
+            {
+                Id = 1,
+                Username = "testusername",
+                Password = "testpassword",
+                Email = "testemail",
+                FirstName = "testfirstname",
+                LastName = "testlastname",
+                Role = new Role()
+                {
+                    Id = 1,
+                    Name = Roles.ApprovedUser
+                },
+                RoleId = 1,
+                Contacts = new List<Contact>()
+                {
+                    new Contact()
+                    {
+                        Id = 1,
+                        MobileNumber = "06 30 555 555",
+                        UserId = 1
+                    }
+                }
+            };
+        }
+    }
+}
