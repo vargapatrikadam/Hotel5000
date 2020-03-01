b5a0450ccdcc6df9c4f09575b89467abfd796dfa
vargapatrikadam <patrikadamvarga@gmail.com>
2020-03-01 17:38:16 +0100

    backend: added empty lodgings controller

---
+++ src/backend/src/Hotel5000_Api/Web/Controllers/LodgingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.Interfaces.LodgingDomain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/lodgings")]
+    [ApiController]
+    public class LodgingsController : ControllerBase
+    {
+        private readonly ILodgingManagementService _lodgingManagementService;
+        private readonly IMapper _mapper;
+        public LodgingsController(ILodgingManagementService lodgingManagementService,
+            IMapper mapper)
+        {
+            _lodgingManagementService = lodgingManagementService;
+            _mapper = mapper;
+        }
+    }
+}
