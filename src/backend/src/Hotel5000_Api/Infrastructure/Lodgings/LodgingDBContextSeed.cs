620f1a7f76209bf2cba2c4f72a907f17255b9790
vargapatrikadam <patrikadamvarga@gmail.com>
2020-03-05 12:56:14 +0100

    backend: added extra seed data for lodgingdb

--- src/backend/src/Hotel5000_Api/Infrastructure/Lodgings/LodgingDBContextSeed.cs
+++ src/backend/src/Hotel5000_Api/Infrastructure/Lodgings/LodgingDBContextSeed.cs
@@ -280,6 +280,48 @@ namespace Infrastructure.Lodgings
                     Name = "Test lodging 2 for preuser2 company",
                     UserId = 2,
                     LodgingTypeId = 2
+                },
+                new Lodging()
+                {
+                    Name = "Test lodging 2 for preuser1 approved user",
+                    UserId = 3,
+                    LodgingTypeId = 2
+                },
+                new Lodging()
+                {
+                    Name = "Test lodging 3 for preuser1 approved user",
+                    UserId = 3,
+                    LodgingTypeId = 2
+                },
+                new Lodging()
+                {
+                    Name = "Test lodging 4 for preuser1 approved user",
+                    UserId = 3,
+                    LodgingTypeId = 2
+                },
+                new Lodging()
+                {
+                    Name = "Test lodging 4 for preuser1 approved user",
+                    UserId = 3,
+                    LodgingTypeId = 2
+                },
+                new Lodging()
+                {
+                    Name = "Test lodging 3 for preuser2 company",
+                    UserId = 2,
+                    LodgingTypeId = 2
+                },
+                new Lodging()
+                {
+                    Name = "Test lodging 4 for preuser2 company",
+                    UserId = 2,
+                    LodgingTypeId = 2
+                },
+                new Lodging()
+                {
+                    Name = "Test lodging 5 for preuser2 company",
+                    UserId = 2,
+                    LodgingTypeId = 2
                 }
             };
         }
@@ -316,6 +358,76 @@ namespace Infrastructure.Lodgings
                     Street = "teststreet3",
                     HouseNumber = "3",
                     LodgingId = 1
+                },
+                new LodgingAddress()
+                {
+                    CountryId = 2,
+                    County = "Nógrád",
+                    City = "Mohora",
+                    PostalCode = "2698",
+                    Street = "Rákóczi út",
+                    HouseNumber = "47",
+                    LodgingId = 4
+                },
+                new LodgingAddress()
+                {
+                    CountryId = 2,
+                    County = "Nógrád",
+                    City = "Balassagyarmat",
+                    PostalCode = "2660",
+                    Street = "Ifjúság útja",
+                    HouseNumber = "18",
+                    LodgingId = 5
+                },
+                new LodgingAddress()
+                {
+                    CountryId = 2,
+                    County = "Nógrád",
+                    City = "Szécsény",
+                    PostalCode = "2660",
+                    Street = "Hársfa út",
+                    HouseNumber = "2",
+                    LodgingId = 6
+                },
+                new LodgingAddress()
+                {
+                    CountryId = 2,
+                    County = "Csongrád",
+                    City = "Szeged",
+                    PostalCode = "6725",
+                    Street = "Szekeres utca",
+                    HouseNumber = "20",
+                    LodgingId = 7
+                },
+                new LodgingAddress()
+                {
+                    CountryId = 2,
+                    County = "Zala",
+                    City = "Zalaegerszeg",
+                    PostalCode = "8900",
+                    Street = "Zárda utca",
+                    HouseNumber = "8",
+                    LodgingId = 8
+                },
+                new LodgingAddress()
+                {
+                    CountryId = 2,
+                    County = "Győr-Moson-Sopron",
+                    City = "Sopron",
+                    PostalCode = "9400",
+                    Street = "Székeles utca",
+                    HouseNumber = "6",
+                    LodgingId = 9
+                },
+                new LodgingAddress()
+                {
+                    CountryId = 2,
+                    County = "Pest",
+                    City = "Budapest",
+                    PostalCode = "1133",
+                    Street = "Ronyva utca",
+                    HouseNumber = "7",
+                    LodgingId = 10
                 }
             };
         }
@@ -379,8 +491,8 @@ namespace Infrastructure.Lodgings
                 },
                 new Room()
                 {
-                    AdultCapacity = 4,
-                    ChildrenCapacity = 2,
+                    AdultCapacity = 1,
+                    ChildrenCapacity = 0,
                     Price = 40,
                     CurrencyId = 1,
                     LodgingId = 3
@@ -397,9 +509,83 @@ namespace Infrastructure.Lodgings
                 {
                     AdultCapacity = 2,
                     ChildrenCapacity = 1,
+                    Price = 10000,
+                    CurrencyId = 2,
+                    LodgingId = 4
+                },
+                new Room()
+                {
+                    AdultCapacity = 1,
+                    ChildrenCapacity = 1,
+                    Price = 9000,
+                    CurrencyId = 2,
+                    LodgingId = 4
+                },
+                new Room()
+                {
+                    AdultCapacity = 2,
+                    ChildrenCapacity = 2,
+                    Price = 20000,
+                    CurrencyId = 2,
+                    LodgingId = 4
+                },
+                new Room()
+                {
+                    AdultCapacity = 2,
+                    ChildrenCapacity = 1,
                     Price = 25,
-                    CurrencyId = 1,
-                    LodgingId = 3
+                    CurrencyId = 2,
+                    LodgingId = 5
+                },
+                new Room()
+                {
+                    AdultCapacity = 1,
+                    ChildrenCapacity = 0,
+                    Price = 15000,
+                    CurrencyId = 2,
+                    LodgingId = 6
+                },
+                new Room()
+                {
+                    AdultCapacity = 2,
+                    ChildrenCapacity = 2,
+                    Price = 30000,
+                    CurrencyId = 2,
+                    LodgingId = 7
+                }
+                ,
+                new Room()
+                {
+                    AdultCapacity = 4,
+                    ChildrenCapacity = 0,
+                    Price = 40000,
+                    CurrencyId = 2,
+                    LodgingId = 7
+                },
+                new Room()
+                {
+                    AdultCapacity = 1,
+                    ChildrenCapacity = 0,
+                    Price = 11000,
+                    CurrencyId = 2,
+                    LodgingId = 8
+                },
+                new Room()
+                {
+                    AdultCapacity = 2,
+                    ChildrenCapacity = 0,
+                    Price = 15000,
+                    CurrencyId = 2,
+                    LodgingId = 9
+                }
+                ,
+                new Room()
+                {
+                    AdultCapacity = 2,
+                    ChildrenCapacity = 0,
+                    Price = 40000,
+                    CurrencyId = 2,
+                    LodgingId = 10
                 }
             };
         }
@@ -425,6 +611,48 @@ namespace Infrastructure.Lodgings
                     To = new DateTime(2020,8,20),
                     LodgingId = 3
                 },
+                new ReservationWindow()
+                {
+                    From = new DateTime(2020,5,1),
+                    To = new DateTime(2020,8,1),
+                    LodgingId = 4
+                },
+                new ReservationWindow()
+                {
+                    From = new DateTime(2020,3,17),
+                    To = new DateTime(2020,3,29),
+                    LodgingId = 5
+                },
+                new ReservationWindow()
+                {
+                    From = new DateTime(2020,9,1),
+                    To = new DateTime(2020,12,21),
+                    LodgingId = 6
+                },
+                new ReservationWindow()
+                {
+                    From = new DateTime(2020,10,20),
+                    To = new DateTime(2020,11,20),
+                    LodgingId = 7
+                },
+                new ReservationWindow()
+                {
+                    From = new DateTime(2020,12,20),
+                    To = new DateTime(2021,8,1),
+                    LodgingId = 8
+                },
+                new ReservationWindow()
+                {
+                    From = new DateTime(2021,1,1),
+                    To = new DateTime(2021,9,1),
+                    LodgingId = 9
+                },
+                new ReservationWindow()
+                {
+                    From = new DateTime(2020,6,1),
+                    To = new DateTime(2020,12,1),
+                    LodgingId = 10
+                }
             };
         }
         private static IEnumerable<ReservationItem> GetPreconfiguredReservationItems()
