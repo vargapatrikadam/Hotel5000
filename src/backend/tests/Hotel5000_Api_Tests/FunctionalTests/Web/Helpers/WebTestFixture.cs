70ad3b1d5e5134bb8fff3a531971c8aa4086887b
vargapatrikadam <patrikadamvarga@gmail.com>
2020-03-08 23:53:45 +0100

    backend: added functional tests for login

---
+++ src/backend/tests/Hotel5000_Api_Tests/FunctionalTests/Web/Helpers/WebTestFixture.cs
@@ -0,0 +1,55 @@
+﻿using Core.Interfaces.PasswordHasher;
+using Infrastructure.Lodgings;
+using Infrastructure.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Web;
+
+namespace Hotel5000_Api_Tests.FunctionalTests.Web.Helpers
+{
+    public class WebTestFixture : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Development");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddEntityFrameworkInMemoryDatabase();
+
+                var provider = services.AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<LoggingDbContext>(options => 
+                {
+                    options.UseInMemoryDatabase("Logging db test database");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                services.AddDbContext<LodgingDbContext>(options => 
+                {
+                    options.UseInMemoryDatabase("Lodging db test database");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                var serviceProvider = services.BuildServiceProvider();
+
+                using (var scope = serviceProvider.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<LodgingDbContext>();
+                    var hasher = scopedServices.GetRequiredService<IPasswordHasher>();
+
+                    db.Database.EnsureCreated();
+
+                    LodgingDbContextSeed.SeedAsync(db, hasher, false).Wait();
+                }
+            });
+        }
+    }
+}
