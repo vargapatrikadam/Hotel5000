93c049819f36fa66cb1c9c97e9f0322aea6fb0ae
vargapatrikadam <patrikadamvarga@gmail.com>
2020-03-08 22:43:32 +0100

    backend: added trust development certifitate to run batch

--- src/backend/run.bat
+++ src/backend/run.bat
@@ -1,4 +1,5 @@
 SET ASPNETCORE_ENVIROMENT = Development
+dotnet dev-certs https --trust
 dotnet restore ./src/Hotel5000_Api/Web/Web.csproj
 dotnet run --project ./src/Hotel5000_Api/Web/Web.csproj --launch-profile Dev --output ./bin --force
 pause
